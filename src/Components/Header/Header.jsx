977c028e8b52572014ee670677439cd6b2d5ff53
digitalamitchoudhary <amitchoudhary.dmp@gmail.com>
2025-02-17 22:52:20 +0530

    dd

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Search from '../Search/Search';
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { IoGitCompareOutline } from "react-icons/io5";
+import { FaRegHeart } from "react-icons/fa6";
+import Tooltip from '@mui/material/Tooltip';
+
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: '0 4px',
+  },
+}));
 
 function Header() {
   return (
@@ -21,7 +38,7 @@ function Header() {
                   <Link
                     to="help-center"
                     className="text-[13px] link font-[500] !transition"
-                  >                  
+                  >
                     Help Center
                   </Link>
                 </li>
@@ -30,8 +47,8 @@ function Header() {
                     to="order-tracking"
                     className="text-[13px] link font-[500] !transition"
                   >
-                
-                   Order Tracking
+
+                    Order Tracking
                   </Link>
                 </li>
               </ul>
@@ -42,16 +59,59 @@ function Header() {
 
       <div className="header !py-3">
         <div className="container flex items-center justify-between">
-            <div className="col1 w-[25%]">
+          <div className="col1 w-[25%]">
 
             <Link to={"/"}> <img src="./logo.jpg" alt="" /></Link>
 
-            </div>
-            <div className="col2 w-[45%]">
+          </div>
+          <div className="col2 w-[45%]">
 
-                <Search/>
-            </div>
-            <div className="col3 w-[30%]">d</div>
+            <Search />
+          </div>
+          <div className="col3 w-[30%] flex items-center pl-7">
+
+            <ul className="flex items-center w-full justify-end gap-3">
+
+              <li>
+                <Link to='/login' className="link transition text-[15px] font-[500]"> Login</Link> /<Link to='/register' className="link transition text-[15px] font-[500]"> Register</Link>
+              </li>
+
+              <li>
+                <Tooltip title="Compare">
+
+                  <IconButton aria-label="cart">
+                    <StyledBadge badgeContent={4} color="secondary">
+                      <IoGitCompareOutline />
+                    </StyledBadge>
+                  </IconButton>
+                </Tooltip >
+
+              </li>
+              <li>
+                <Tooltip title="Wishlist">
+
+                  <IconButton aria-label="cart">
+                    <StyledBadge badgeContent={4} color="secondary">
+                      <FaRegHeart />
+                    </StyledBadge>
+                  </IconButton>
+                </Tooltip >
+
+              </li>
+              <li>
+                <Tooltip title="Cart">
+
+                  <IconButton aria-label="cart">
+                    <StyledBadge badgeContent={4} color="secondary">
+                      <MdOutlineShoppingCart />
+                    </StyledBadge>
+                  </IconButton>              </Tooltip>
+
+              </li>
+
+            </ul>
+
+          </div>
         </div>
 
 
--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import'./Search.css'
+import Button from '@mui/material/Button';
+import { IoSearch } from "react-icons/io5";
+
 
 function Search() {
   return (
-    <div className="searchBox "></div>
+<div className="searchBox w-[100%] h-[50px] bg-[#e5e5e5] rounded-[5px] relative !p-2">
+
+<input type="text" placeholder='Search for Product....' className='w-full h-[35px] focus:outline-none  !p-2 text-[15px]'/>
+<Button className='!absolute !top-[8px] right-[5px] z-50 !w-[37px] !min-w-[37px] h-[35px] !rounded-full !text-black'>< IoSearch className='text-[22px] text-[#4e4e4e]' />
+</Button>
+
+</div>
   )
 }
 
