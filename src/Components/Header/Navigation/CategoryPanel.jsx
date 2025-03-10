4430dcb0750a4a760ca2d09839d42a04c4035673
rahulsharma180 <rahulsharma18072022@gmail.com>
2025-03-10 17:45:37 +0530

    s

--- src/Components/Header/Navigation/CategoryPanel.jsx
+++ src/Components/Header/Navigation/CategoryPanel.jsx
@@ -5,13 +5,16 @@ import Button from '@mui/material/Button';
 import { IoCloseSharp } from 'react-icons/io5';
 import { FaRegSquarePlus } from 'react-icons/fa6';
 import   "../Navigation/styles.css"
+import { Link } from 'react-router-dom';
 function CategoryPanel(props) {
   const toggleDrawer = (newOpen) => () => {
     props.setIsOpenCatPanel(newOpen);
   };
 
   const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)} className="categoryPanel button" >
+    <Box sx={{ width: 250 }} role="presentation"
+    // onClick={toggleDrawer(false)}
+    className="categoryPanel button" >
       <h3 className="!p-3 text-[16px] font-[500] flex item-center justify-between">
         {' '}
         Shop By Categories{' '}
@@ -24,19 +27,33 @@ function CategoryPanel(props) {
       <div className="scroll">
         <ul className="w-full">
           <li className="list-none flex items-center relative">
+          <Link to="/" className="w-full">
             <Button className="w-full !text-left !justify-start !px-3 !text-[rgb(0,0,0,0.8)]">
               Fashion
             </Button>
+            </Link>
             <FaRegSquarePlus className="absolute top-[10px] right-[15px] cursor-pointer" />
 
             <ul className="submenu absolute top-[100%] left-[0%] w-full !pl-3">
-              {' '}
+               
               <li className="list-none relative ">
+              <Link to="/" className="w-full">
                 <Button className="w-full !text-left !justify-start !px-3 !text-[rgb(0,0,0,0.8)]">
                   Apparel
                 </Button>
+                </Link>
+
                 <FaRegSquarePlus className="absolute top-[10px] right-[15px] cursor-pointer" />
-              </li>{' '}
+                <ul className="submenu absolute top-[100%] left-[0%] w-full !pl-3">
+               
+               <li className="list-none relative ">
+                 <Link to="/" className="link w-full !text-left !justify-start !px-3 transition text-[14px]">
+                   Apparel
+                 </Link>
+                 </li> 
+             </ul>
+             
+              </li> 
             </ul>
           </li>
         </ul>
