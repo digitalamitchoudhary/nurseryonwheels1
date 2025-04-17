5e2cf7be214cda14ad78ae5c6a2930bce40b52b7
rahulsharma180 <rahulsharma18072022@gmail.com>
2025-04-17 18:01:20 +0530

    aa

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-
+import React from 'react';
+import '../Sidebar/Sidebar.css';
+ import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 function Sidebar() {
   return (
-    <div>
-      
+    <div className="sidebar !py-5">
+      <div className="box">
+        <h3 className="mb-3 text-[16px] font-[500]"> Shop by Category</h3> 
+        <div className="scroll !px-3 relative -left-[10px]">
+       
+         <FormControlLabel control={<Checkbox size="small" />} label="Fashion" className='w-full'/>
+        <FormControlLabel control={<Checkbox size="small" />} label="Electroinc"className='w-full'/>
+        <FormControlLabel control={<Checkbox size="small" />} label="Bags" className='w-full'/>
+        <FormControlLabel control={<Checkbox size="small" />} label="Footwear" className='w-full'/>
+        <FormControlLabel control={<Checkbox size="small" />} label="Groceries" className='w-full'/>
+        <FormControlLabel control={<Checkbox size="small" />} label="Beauty" className='w-full'/>
+        <FormControlLabel control={<Checkbox size="small" />} label="Wellness" className='w-full'/>
+        <FormControlLabel control={<Checkbox  size="small"/>} label="Jewellery" className='w-full'/>
+
+
+        
+        </div>  
+      </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
--- src/Pages/ProductListing/ProductListing.jsx
+++ src/Pages/ProductListing/ProductListing.jsx
@@ -1,12 +1,39 @@
-import { ClassNames } from '@emotion/react'
-import React from 'react'
+ import React from 'react'
+import Sidebar from '../../Components/Sidebar/Sidebar'
+import Typography from '@mui/material/Typography';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
 
 function ProductListing() {
   return (
     <div className='!py-8'>
-        <div className="container">
-            fdf
+      <div className="container">
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link underline="hover" color="inherit"      className='link transition '
+ href="/">
+         Home
+        </Link>
+        <Link
+          underline="hover"
+          color="inherit"
+          href="/ "
+          className='link transition'
+        >
+          Fashion
+        </Link>
+       </Breadcrumbs>
+      </div>
+
+
+      <div className="bg-white !p-2 mt-4">
+      <div className="container flex gap-3">
+        
+        <div className="sidebarWrapper bg-white w-[20%] h-full  ">
+          <Sidebar/>
         </div>
+        </div>
+      </div>
+         
     </div>
   )
 }
