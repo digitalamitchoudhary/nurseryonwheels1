80bc7a9e92340c92ffc0ff53a5cc7d8990b5b75a
rahulsharma180 <rahulsharma18072022@gmail.com>
2025-06-03 17:51:26 +0530

    dd

--- src/Pages/ProductListing/ProductListing.jsx
+++ src/Pages/ProductListing/ProductListing.jsx
@@ -1,64 +1,73 @@
- import React from 'react'
-import Sidebar from '../../Components/Sidebar/Sidebar'
+import React from 'react';
+import Sidebar from '../../Components/Sidebar/Sidebar';
 import Typography from '@mui/material/Typography';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Link from '@mui/material/Link';
 import '../ProductListing/Product.css';
+import { IoGridSharp } from 'react-icons/io5';
+import { LuMenu } from 'react-icons/lu';
 
 import ProductItem from '../../Components/ProductItem/ProductItem';
+import { Button } from '@mui/material';
 function ProductListing() {
   return (
-    <div className='!py-8'>
+    <div className="!py-8">
       <div className="container">
-      <Breadcrumbs aria-label="breadcrumb"> 
-        <Link underline="hover" color="inherit"      className='link transition '
- href="/">
-         Home
-        </Link>
-        <Link
-          underline="hover"
-          color="inherit"
-          href="/ "
-          className='link transition'
-        >
-          Fashion
-        </Link>
-       </Breadcrumbs>
+        <Breadcrumbs aria-label="breadcrumb">
+          <Link
+            underline="hover"
+            color="inherit"
+            className="link transition "
+            href="/"
+          >
+            Home
+          </Link>
+          <Link
+            underline="hover"
+            color="inherit"
+            href="/ "
+            className="link transition"
+          >
+            Fashion
+          </Link>
+        </Breadcrumbs>
       </div>
-
 
       <div className="bg-white !p-2 mt-4">
-      <div className="container flex gap-3">
-        
-        <div className="sidebarWrapper bg-white w-[20%] h-full  ">
-          <Sidebar/>
-        </div>
+        <div className="container flex gap-3">
+          <div className="sidebarWrapper bg-white w-[20%] h-full  ">
+            <Sidebar />
+          </div>
 
-        <div className=" rightContent   w-[80%]">
+          <div className=" rightContent   w-[80%]">
+            <div className=" bg-[#f1f1f1]  !p-2 w-full !mb-3 rounded-md flex items-center">
+              
 
+              <div className="col1 flex items-center ">
+               
+                <Button className="!w-[40px] !h-[40px] !min-w-[40px] !rounded-full !text-[#000]">
+                  <LuMenu className="text-[rgba(0,0,0,0.7)]" />
+                </Button> <Button className="!w-[40px] !h-[40px] !min-w-[40px] !rounded-full !text-[#000]">
+                  <IoGridSharp className="text-[rgba(0,0,0,0.7)]" />
+                </Button>
+              </div>
+            </div>
 
-        <div className="grid pro  grid-cols-4 md:grid-cols-4 gap-4">
-          
-        <ProductItem/>
-        <ProductItem/>
-        <ProductItem/>
-        <ProductItem/>
-        <ProductItem/>
-        <ProductItem/>
-        <ProductItem/>
-        <ProductItem/>
-
-        </div>
-
-        </div>
-  
-  
-
+            <div className="grid pro  grid-cols-4 md:grid-cols-4 gap-4">
+              <ProductItem />
+              <ProductItem />
+              <ProductItem />
+              <ProductItem />
+              <ProductItem />
+              <ProductItem />
+              <ProductItem />
+              <ProductItem />
+            </div>
+          </div>
         </div>
       </div>
-         
     </div>
-  )
+  );
 }
 
-export default ProductListing
+export default ProductListing;
