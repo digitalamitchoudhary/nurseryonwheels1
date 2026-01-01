53f1ef81e22ba2a920bab5d0d634fd0655b5e649
Digitalamitchoudhary <amitchoudhary.dmp@gmail.com>
2026-01-01 23:42:38 +0530

    dd

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -216,92 +216,101 @@ function ProductDetails() {
  className="!px-6 !py-4 font-medium text-heading whitespace-nowrap"
  >
 Apple MacBook Pro 17&quot;
-        </th>
-        <td className="!px-6 !py-4">Silver</td>
-        <td className="!px-6 !py-4">Laptop</td>
-        <td className="!px-6 !py-4">$2999</td>
-        <td className="!px-6 !py-4">
-          <a href="#" className="font-medium text-fg-brand hover:underline">
-            Edit
-          </a>
-        </td>
-      </tr>
+        </th>
+        <td className="!px-6 !py-4">Silver</td>
+        <td className="!px-6 !py-4">Laptop</td>
+        <td className="!px-6 !py-4">$2999</td>
+        <td className="!px-6 !py-4">
+          <a href="#" className="font-medium text-fg-brand hover:underline">
+            Edit
+          </a>
+        </td>
+      </tr>
 
-      <tr className="odd:bg-neutral-primary even:bg-neutral-secondary-soft border-b border-default">
-        <th
-          scope="row"
-          className="!px-6 !py-4 font-medium text-heading whitespace-nowrap"
-        >
-          Microsoft Surface Pro
-        </th>
-        <td className="!px-6 !py-4">White</td>
-        <td className="!px-6 !py-4">Laptop PC</td>
-        <td className="!px-6 !py-4">$1999</td>
-        <td className="!px-6 !py-4">
-          <a href="#" className="font-medium text-fg-brand hover:underline">
-            Edit
-          </a>
-        </td>
-      </tr>
+      <tr className="odd:bg-neutral-primary even:bg-neutral-secondary-soft border-b border-default">
+        <th
+          scope="row"
+          className="!px-6 !py-4 font-medium text-heading whitespace-nowrap"
+        >
+          Microsoft Surface Pro
+        </th>
+        <td className="!px-6 !py-4">White</td>
+        <td className="!px-6 !py-4">Laptop PC</td>
+        <td className="!px-6 !py-4">$1999</td>
+        <td className="!px-6 !py-4">
+          <a href="#" className="font-medium text-fg-brand hover:underline">
+            Edit
+          </a>
+        </td>
+      </tr>
 
-      <tr className="odd:bg-neutral-primary even:bg-neutral-secondary-soft border-b border-default">
-        <th
-          scope="row"
-          className="!px-6 !py-4 font-medium text-heading whitespace-nowrap"
-        >
-          Magic Mouse 2
-        </th>
-        <td className="!px-6 !py-4">Black</td>
-        <td className="!px-6 !py-4">Accessories</td>
-        <td className="!px-6 !py-4">$99</td>
-        <td className="!px-6 !py-4">
-          <a href="#" className="font-medium text-fg-brand hover:underline">
-            Edit
-          </a>
-        </td>
-      </tr>
+      <tr className="odd:bg-neutral-primary even:bg-neutral-secondary-soft border-b border-default">
+        <th
+          scope="row"
+          className="!px-6 !py-4 font-medium text-heading whitespace-nowrap"
+        >
+          Magic Mouse 2
+        </th>
+        <td className="!px-6 !py-4">Black</td>
+        <td className="!px-6 !py-4">Accessories</td>
+        <td className="!px-6 !py-4">$99</td>
+        <td className="!px-6 !py-4">
+          <a href="#" className="font-medium text-fg-brand hover:underline">
+            Edit
+          </a>
+        </td>
+      </tr>
 
-      <tr className="odd:bg-neutral-primary even:bg-neutral-secondary-soft border-b border-default">
-        <th
-          scope="row"
-          className="!px-6 !py-4 font-medium text-heading whitespace-nowrap"
-        >
-          Google Pixel Phone
-        </th>
-        <td className="!px-6 !py-4">Gray</td>
-        <td className="!px-6 !py-4">Phone</td>
-        <td className="!px-6 !py-4">$799</td>
-        <td className="!px-6 !py-4">
-          <a href="#" className="font-medium text-fg-brand hover:underline">
-            Edit
-          </a>
-        </td>
-      </tr>
+      <tr className="odd:bg-neutral-primary even:bg-neutral-secondary-soft border-b border-default">
+        <th
+          scope="row"
+          className="!px-6 !py-4 font-medium text-heading whitespace-nowrap"
+        >
+          Google Pixel Phone
+        </th>
+        <td className="!px-6 !py-4">Gray</td>
+        <td className="!px-6 !py-4">Phone</td>
+        <td className="!px-6 !py-4">$799</td>
+        <td className="!px-6 !py-4">
+          <a href="#" className="font-medium text-fg-brand hover:underline">
+            Edit
+          </a>
+   </td>
+      </tr>
 
-      <tr className="odd:bg-neutral-primary even:bg-neutral-secondary-soft">
-        <th
-          scope="row"
-          className="!px-6 !py-4 font-medium text-heading whitespace-nowrap"
-        >
-          Apple Watch 5
-        </th>
-        <td className="!px-6 !py-4">Red</td>
-        <td className="!px-6 !py-4">Wearables</td>
-        <td className="!px-6 !py-4">$999</td>
-        <td className="!px-6 !py-4">
-          <a href="#" className="font-medium text-fg-brand hover:underline">
-            Edit
-          </a>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+      <tr className="odd:bg-neutral-primary even:bg-neutral-secondary-soft">
+        <th
+          scope="row"
+          className="!px-6 !py-4 font-medium text-heading whitespace-nowrap"
+        >
+          Apple Watch 5
+        </th>
+        <td className="!px-6 !py-4">Red</td>
+        <td className="!px-6 !py-4">Wearables</td>
+        <td className="!px-6 !py-4">$999</td>
+        <td className="!px-6 !py-4">
+          <a href="#" className="font-medium text-fg-brand hover:underline">
+            Edit
+          </a>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </div>
                     </div>
                   )}
                   {activeTab==="review" && (
                     <div className="!mt-4">
                       <p>Review content goes here.</p>
+
+                    <div className="shadow-md w-full !py-5 !px-8 rounded-md text-[14px] bg-gray-50 !mt-4">
+                        <div className=" border-red-400  border-2 w-full productReviwerContainer">
+                          <div className="scroll !m-5 border-2  w-[70%]       ">s
+                            
+                          </div>
+                        </div>
+                      </div>
+
                     </div>
                   )}
         </div>
